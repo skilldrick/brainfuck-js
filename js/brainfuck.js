09726e63a267292a3995b7d395f865adf52d08e3
Nick Morgan <nickm@twitter.com>
2012-11-01 16:41:55 -0700

    Make input and output closure-scoped rather than passed around.

--- js/brainfuck.js
+++ js/brainfuck.js
@@ -1,22 +1,24 @@
 var parse = (function () {
+  var input;
+  var output;
   var data;
   var ptr;
   var debug = false;
 
   var ops = {
-    '+': function (output) {
+    '+': function () {
       data[ptr] = data[ptr] || 0;
       data[ptr]++;
       debug && console.log('+', data[ptr], ptr);
     },
 
-    '-': function (output) {
+    '-': function () {
       data[ptr] = data[ptr] || 0;
       data[ptr]--;
       debug && console.log('-', data[ptr], ptr);
     },
 
-    '<': function (output) {
+    '<': function () {
       ptr--;
       if (ptr < 0) {
         ptr = 0; //Don't allow pointer to leave data array
@@ -24,18 +26,18 @@ var parse = (function () {
       debug && console.log('<', ptr);
     },
 
-    '>': function (output) {
+    '>': function () {
       ptr++;
       debug && console.log('>', ptr);
     },
 
-    '.': function (output) {
+    '.': function () {
       var c = String.fromCharCode(data[ptr]);
       output.push(c);
       debug && console.log('.', c, data[ptr]);
     },
 
-    ',': function (output, input) {
+    ',': function () {
       var c = input.shift();
       if (typeof c == "string") {
         data[ptr] = c.charCodeAt(0);
@@ -45,15 +47,15 @@ var parse = (function () {
   };
 
   function program(nodes) {
-    return function (input) {
-      var output = [];
+    return function (inputString) {
+      output = [];
       data = [];
       ptr = 0;
 
-      input = input && input.split('') || [];
+      input = inputString && inputString.split('') || [];
 
       nodes.forEach(function (node) {
-        node(output, input);
+        node();
       });
 
       return output.join('');
@@ -61,15 +63,14 @@ var parse = (function () {
   }
 
   function loop(nodes) {
-    return function (output, input) {
+    return function () {
       while(data[ptr] > 0) {
         nodes.forEach(function (node) {
-          node(output, input);
+          node();
         });
       }
     };
   }
-
 
 
 
